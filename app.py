89c47373130198cd4d58dc62bc667425eb86967a
Abdelrahman Sadawy <asadawy110@gmail.com>
2023-07-04 01:00:12 +0300

    Revert "Updated the icons"
    This reverts commit 865d7d8fa68ab7c2487c17fc6874432fa390228f.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="SentiScribe", page_icon='https://i.imgur.com/eP2jxYt.png', layout="wide")
+st.set_page_config(page_title="SentiScribe", page_icon='https://cdn-icons-png.flaticon.com/512/8637/8637099.png', layout="wide")
 import user_reviews_page
 
 user_reviews_page.main_menu()
